741f47c4a08c3915773d06d064e51f728dbc0ccb
Leonardo Grupioni <52112271+leonardogrupioni@users.noreply.github.com>
2024-05-26 15:08:25 -0300

    Add files via upload

---
+++ PUC/CadastroAlunos/Aplicacao.java
@@ -0,0 +1,14 @@
+
+/**
+ * Escreva uma descrição da classe Aplicacao aqui.
+ * 
+ * @author Julio Arakaki 
+ * @version 28/03/2023
+ */
+public class Aplicacao {
+
+    public static void main(String args[]) {
+        CadastroAlunos cad = new CadastroAlunos(60);
+    
+    }
+}
